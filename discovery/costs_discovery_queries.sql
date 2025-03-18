c410dba3d14ae1a2e014115b7cba8eb73917efd5
Donald Futers <donald.futers@gmail.com>
2025-03-18 00:02:01 +0000

    🧐 save Snowflake SQL queries used in inspection of reconditioning costs data

---
+++ discovery/costs_discovery_queries.sql
@@ -0,0 +1,79 @@
+use role necars_role;
+
+with window_table as (
+    select
+        stock_number || ' ' || date || ' ' || description as composite_key,
+        count(composite_key) over(partition by composite_key) as instance_count
+    from
+        reconditioning_cost
+)
+select
+    *
+from
+    window_table
+where instance_count > 1
+order by instance_count desc;
+
+select *
+from reconditioning_cost
+where stock_number = 33569
+  and date = '30/06/2023 00:00';
+
+with window_table as (
+    select
+        sage_ref,
+        count(sage_ref) over(partition by sage_ref) as total
+    from
+        reconditioning_cost
+)
+select *
+from window_table
+--where total > 1
+order by total desc;
+
+select description,
+    avg(net_cost_price)
+from reconditioning_cost
+group by description;
+
+select description,
+    count(*) as total
+from reconditioning_cost
+where description ilike '%warranty%'
+  or description ilike '%platinum%'
+group by description
+order by 2 desc;
+
+with counted as (
+    select description,
+        count(*) as total
+    from reconditioning_cost
+    where description ilike '%warranty%'
+        or description ilike '%platinum%'
+    group by description
+    order by 2 desc
+)
+select description, total,
+    sum(total) over() as cumulative_total
+from counted;
+
+select stock_number,
+    count(stock_number) as total
+from reconditioning_cost
+group by stock_number
+order by total desc;
+
+select distinct stock_number, nominal_code
+from reconditioning_cost
+where stock_number = 34401;
+
+select distinct
+    description,
+    nominal_code
+from reconditioning_cost
+order by nominal_code;
+
+select count(*)
+from reconditioning_cost
+where description = 'BUYERS FEE';
+
