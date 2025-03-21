dadb9d1c358d51f4189caefb9fcbfda14888ac6f
Donald Futers <donald.futers@gmail.com>
2025-03-21 17:44:01 +0000

    🔨 implement int_vehicle model, an intermediate step toward construction of dim_vehicle table

---
+++ necars_dbt/models/marts/int_vehicle.sql
@@ -0,0 +1,7 @@
+select
+  cast(substring(full_stock_number, 2) as int) as stock_id,
+  substring(full_stock_number, 1, 1) as stock_type,
+  make,
+  vehicle_description
+from
+  {{ ref('stg_source_vehicle') }} as src_vehicle
