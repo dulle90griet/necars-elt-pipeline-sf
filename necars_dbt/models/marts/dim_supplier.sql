64653831aa7a1356bf826a3a4da74b02cb54c825
Donald Futers <donald.futers@gmail.com>
2025-04-03 17:21:15 +0100

    🎨 separate remaining top-level subquery in dim_supplier model into int_supplier_first_appearance model

--- necars_dbt/models/marts/dim_supplier.sql
+++ necars_dbt/models/marts/dim_supplier.sql
@@ -2,19 +2,4 @@ select
     -- Generate supplier_ids in order of first appearance
     row_number() over (order by first_appearance) as supplier_id
     ,supplier as supplier_name
-from (
-    select
-        -- Retain the suppliers with their earliest appearances
-        case
-            when recondition_date < purchase_date
-              or purchase_date is null
-              then recondition_date
-            else purchase_date
-        end as first_appearance
-        ,supplier
-    from {{ ref('int_distinct_supplier') }} as s
-    left join {{ ref('int_supplier_first_appearance_cost') }} as c
-      on recondition_supplier = supplier
-    left join {{ ref('int_supplier_first_appearance_vehicle') }} as v
-      on vehicle_supplier = supplier
-) supplier_appearance
+from {{ ref('int_supplier_first_appearance') }}
