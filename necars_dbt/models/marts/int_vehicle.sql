7e6802d3dfa8a280cf45c6a462ccacb2852a2481
Donald Futers <donald.futers@gmail.com>
2025-04-17 18:28:36 +0100

    🔨 update int_vehicle model to normalize inconsistent manufacturer name orthography (BMW, DS Automobiles, Skoda) in source data

--- necars_dbt/models/marts/int_vehicle.sql
+++ necars_dbt/models/marts/int_vehicle.sql
@@ -1,7 +1,12 @@
 select
   cast(substring(full_stock_number, 2) as int) as stock_id
   ,substring(full_stock_number, 1, 1) as stock_type
-  ,make
+  ,case
+    when make ilike 'bmw' then 'BMW'
+    when make ilike 'ds%' then 'DS Automobiles'
+    when make ilike 'skoda' then 'Skoda'
+    else make
+  end as make
   ,vehicle_description
 from
   {{ ref('stg_source_vehicle') }} as src_vehicle
